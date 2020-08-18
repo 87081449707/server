be8f91a1dc5245eecb386feb0ad220fd79fd7c69
87081449707 <87081449707@mail.ru>
2020-08-18 15:26:11 +0600

    1

--- main.js
+++ main.js
@@ -4,38 +4,43 @@ console.log('Hello world')
 var server
 var server_id
 
-server_connect = setInterval(function() {
+server_connect = setInterval(function(){
   server = new Peer()
 
-  server.on('open', function(id) {
-    document.getElementById('log').innerHTML += 'peer server id: ' + id
+  server.on('open', function(id){
+    document.getElementById('log').innerHTML += 'peerJs server id: ' + id
+    
+    server_id = id
     
     clearInterval(server_connect)
   })
 
   server.on('error', function(error) {
-    document.getElementById('log').innerHTML += 'peer server error: ' + error
+    document.getElementById('log').innerHTML += 'peerJs server error: ' + error
   })
   
-  server.on('connection', function(connect) {
+  server.on('connection', function(connect){
+    document.getElementById('log').innerHTML += JSON.parce(connect)
+    
     connect.on('data', function(data) {
       server_receive(JSON.parse(data))
+      
     })
   })
 }, 1000)
 
-server_send = function (data){
-  document.getElementById('log').innerHTML += 'send: ' + data
+server_send = function (id, data){
+  document.getElementById('log').innerHTML += 'peerJs server send: ' + 'id: ' + id + ' ' + 'data: ' + data
   
-  server.send(JSON.stringify(data))
+  //server.send(JSON.stringify(data))
 }
 server_receive = function (data){
-  document.getElementById('log').innerHTML += 'receive: ' + data
+  document.getElementById('log').innerHTML += 'peerJs server receive: ' + data
 }
 
 // touchpad
 document.addEventListener('touchstart', function(event) {
-
+  document.getElementById('log').innerHTML += server_id
 }, false)
 
 // party
