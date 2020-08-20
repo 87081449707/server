82438ea2e8162685d25227b71898a883d6774351
87081449707 <68937740+87081449707@users.noreply.github.com>
2020-08-20 15:29:29 +0600

    update main.js

--- main.js
+++ main.js
@@ -72,13 +72,14 @@ var z = []
 
 z[0] = 0
 z[1] = 1
+z[z.length] = 2
 
 for (var i = 0; i <= z.length; i++){
   console.log(i)
   
   if (i == z.length){
-    //z[i] = z.length
+    //z[z.length] = []
     
-    //console.log(z)
+    console.log(z)
   }
 }
