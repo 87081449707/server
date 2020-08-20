7319693ff39f3cf00e0415588aa578d553fbaaf9
87081449707 <68937740+87081449707@users.noreply.github.com>
2020-08-20 17:18:34 +0600

    update main.js

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ console.log('Hello world')
 var server
 var server_id
 
-server_connect = setInterval(function(){
+server_connect = setInterval(function() {
   server = new Peer()
 
-  server.on('open', function(id){
-    document.getElementById('log').innerHTML += '<br> peerJs server open id: ' + id
-    
+  server.on('open', function(id) {
+    console.log('peerJs server open id: ' + id)
+
     server_id = id
-    
+
     clearInterval(server_connect)
   })
 
   server.on('error', function(error) {
-    document.getElementById('log').innerHTML += '<br> peerJs server error: ' + error
-    
+    console.log('peerJs server error: ' + error)
+
     clearInterval(server_connect)
   })
-  
-  server.on('connection', function(connect){
+
+  server.on('connection', function(connect) {
     connect.on('data', function(data) {
-      document.getElementById('log').innerHTML += '<br> peerJs data: ' + data
-      
+      console.log('peerJs data: ' + data)
+
       connect.send('hi')
-      
-        party_my(data)
-        connect.send(party_other(data))
+
+      party_my(data)
+      connect.send(party_other(data))
     })
   })
 }, 2000)
@@ -41,37 +41,41 @@ document.addEventListener('touchstart', function(event) {
 // party
 var party = []
 
-party_my = function (data){
-  document.getElementById('log').innerHTML += '<br> party: ' + data
-  
-  for (var i = 0; i < party.length; i++){
-    if (party[i].id == data.id){
-      return
-    }
-    if (i == party.length - 1){
-      party[i] = {id : data.id, geolocation : {x : data.deolocation.x, y : data.geolocation.y}}
+party_data = function (data) {
+  if (data.party == 'other') {
+    var array = []
+
+    for (var i = 0; i < party.length; i++) {
+      var distance = Math.sqrt(Math.pow(data.geolocation.x - party.geolocation.x, 2) + Math.pow(data.geolocation.y - party.geolocation.y, 2))
+
+      if (distance <= party[i]) {
+        array.splice(i, 0, distance)
+        array.length = data.number
+
+        return array
+      }
     }
   }
-}
-party_other = function (data){
-  var array = []
-  
-  for (var i = 0; i < party.length; i++){
-    var distance = Math.sqrt(Math.pow(data.geolocation.x - party.geolocation.x, 2) + Math.pow(data.geolocation.y - party.geolocation.y, 2))
-    
-    if (distance <= party[i]){
-      array.splice(i, 0, distance)
-      array.length = data.number
-      
-      return array
+  if (data.party == 'my') {
+    for (var i = 0; i <= party.length; i++) {
+      if (i == party.length) {
+        party[i] = {
+          id: data.id,
+          geolocation: {
+            x: data.deolocation.x,
+            y: data.geolocation.y
+          }
+        }
+        break
+      }
     }
   }
 }
 
 var z = []
 
-for (var i = 0; i <= z.length; i++){
-  if (i == z.length){
+for (var i = 0; i <= z.length; i++) {
+  if (i == z.length) {
     z[i] = i
     console.log(z)
     break
